4542b5ca0cd00c8b3b785839af0b1e9ef220d356
Aishiia <aishiia278@gmail.com>
2023-11-29 14:17:30 +0700

    adding activity_detail.xml

---
+++ app/src/main/java/com/teamfan/plantpedia/ui/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.teamfan.plantpedia.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamfan.plantpedia.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
