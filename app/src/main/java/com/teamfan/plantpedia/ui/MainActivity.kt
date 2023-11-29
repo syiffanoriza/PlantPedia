55b1cbd0dae067595762407bd77e5298d35ea7e3
syiffanoriza <syiffa.noriza@gmail.com>
2023-11-29 09:35:38 +0700

    Refactoring Project Packages

--- app/src/main/java/com/teamfan/plantpedia/ui/MainActivity.kt
+++ app/src/main/java/com/teamfan/plantpedia/ui/MainActivity.kt
@@ -1,12 +1,12 @@
-package com.teamfan.plantpedia
+package com.teamfan.plantpedia.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.FAN.plantpedia.R
+import com.teamfan.plantpedia.R
 
-class SearchableActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_searchable)
+        setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/teamfan/plantpedia/ui/SearchableActivity.kt
+++ app/src/main/java/com/teamfan/plantpedia/ui/SearchableActivity.kt
@@ -1,11 +1,12 @@
-package com.teamfan.plantpedia
+package com.teamfan.plantpedia.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.teamfan.plantpedia.R
 
-class MainActivity : AppCompatActivity() {
+class SearchableActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_searchable)
     }
 }
