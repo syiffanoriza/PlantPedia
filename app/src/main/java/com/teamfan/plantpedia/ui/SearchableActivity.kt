45192dbc44e42d9162d2616f01c3ffec7b9dfa27
syiffanoriza <syiffa.noriza@gmail.com>
2023-11-29 14:15:33 +0700

    Project Update, BookAdapter and book_item.xml

--- app/src/main/java/com/teamfan/plantpedia/ui/SearchableActivity.kt
+++ app/src/main/java/com/teamfan/plantpedia/ui/SearchableActivity.kt
@@ -2,7 +2,6 @@ package com.teamfan.plantpedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.FAN.plantpedia.R
 
 class SearchableActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
