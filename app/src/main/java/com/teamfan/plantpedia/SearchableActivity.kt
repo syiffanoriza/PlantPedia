6c7c235ba663d9d0a34a38d031c6fe00014575dd
firasyaalya <firasyaalya9@gmail.com>
2023-11-29 09:20:46 +0700

    Ui update and add logo update

---
+++ app/src/main/java/com/teamfan/plantpedia/SearchableActivity.kt
@@ -0,0 +1,12 @@
+package com.teamfan.plantpedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.FAN.plantpedia.R
+
+class SearchableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_searchable)
+    }
+}
