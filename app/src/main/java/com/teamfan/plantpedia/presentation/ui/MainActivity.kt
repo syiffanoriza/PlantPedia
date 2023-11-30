f38f80d22979a378f502187edec351b67c2bfd96
syiffanoriza <syiffa.noriza@gmail.com>
2023-11-30 07:40:43 +0700

    Layouting and Networking for MainActivity, SearchableActivity

--- app/src/main/java/com/teamfan/plantpedia/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/teamfan/plantpedia/presentation/ui/MainActivity.kt
@@ -4,22 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.teamfan.plantpedia.R
+import com.teamfan.plantpedia.adapter.BookAdapter
 import com.teamfan.plantpedia.databinding.ActivityMainBinding
+import com.teamfan.plantpedia.network.BookItem
 import com.teamfan.plantpedia.presentation.BooksViewModel
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding as ActivityMainBinding
+
     private var _bookViewModel: BooksViewModel? = null
     private val bookViewModel get() = _bookViewModel as BooksViewModel
+    private val list = ArrayList<BookItem>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        _bookViewModel = ViewModelProvider(this)[BooksViewModel::class.java]
-        bookViewModel.showBooks()
+        initView()
+     }
 
+    private fun initView() {
+        binding.rvBooks.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvBooks.adapter = BookAdapter(list)
     }
 }
