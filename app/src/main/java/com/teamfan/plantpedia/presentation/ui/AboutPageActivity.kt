55a61df6adb87be610bb7bba59416da8e6fc24c5
Aishiia <aishiia278@gmail.com>
2023-12-03 09:46:43 +0700

    adding ic_info and btnGoToGithub in AboutPageActivity.kt

--- app/src/main/java/com/teamfan/plantpedia/presentation/ui/AboutPageActivity.kt
+++ app/src/main/java/com/teamfan/plantpedia/presentation/ui/AboutPageActivity.kt
@@ -1,12 +1,26 @@
 package com.teamfan.plantpedia.presentation.ui
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.teamfan.plantpedia.R
 
 class AboutPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_page)
+
+        val btnOpenUrl: Button = findViewById(R.id.btn_github)
+
+        btnOpenUrl.setOnClickListener {
+            val openUrl = Intent(android.content.Intent.ACTION_VIEW)
+            openUrl.data = Uri.parse("https://github.com/syiffanoriza/PlantPedia")
+
+            startActivity(openUrl)
+        }
     }
+
+
 }
