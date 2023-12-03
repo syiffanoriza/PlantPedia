8c3d6e18ee896182ec0919a3a9379b6b5eec041f
syiffanoriza <syiffa.noriza@gmail.com>
2023-12-03 11:13:17 +0700

    Enabled AppBar Toolbar Features

--- app/src/main/java/com/teamfan/plantpedia/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/teamfan/plantpedia/presentation/ui/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.teamfan.plantpedia.presentation.ui
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.teamfan.plantpedia.R
 import com.teamfan.plantpedia.databinding.ActivityMainBinding
@@ -15,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
         val fragmentManager = supportFragmentManager
         val bookFragment = BookFragment()
@@ -24,4 +31,26 @@ class MainActivity : AppCompatActivity() {
             .add(R.id.fl_books, bookFragment, BookFragment::class.java.simpleName)
             .commit()
      }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu?.findItem(R.id.option_search)?.actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.option_info){
+            val intent = Intent(this, AboutPageActivity::class.java)
+            startActivity(intent)
+        }
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
