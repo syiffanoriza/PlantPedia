a5980e57ccb5b9bf626e8fd9f4bb60010b2f0abc
firasyaalya <firasyaalya9@gmail.com>
2023-12-03 08:52:40 +0700

    Add AboutPageActivity and activity_about_page.xml

---
+++ app/src/main/java/com/teamfan/plantpedia/presentation/ui/AboutPageActivity.kt
@@ -0,0 +1,12 @@
+package com.teamfan.plantpedia.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamfan.plantpedia.R
+
+class AboutPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_page)
+    }
+}
